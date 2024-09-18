47c3dd73531b2f9e642deaf0704bbba42f48dd76
priyosu <suxpriyo@gmail.com>
2024-09-18 13:53:48 +0600

    added function

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 let display = document.querySelector(".container input");
-let keys = document.querySelector(".keys").querySelectorAll(button);
-console.log(display.placeholder);
+let keys = document.querySelector(".keys").querySelectorAll("button");
+
+keys.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.value);
+    display.value = display.value + btn.value;
+  });
+});
+
+function clearScreen() {
+  display.value = "";
+}
+
+function ansWer() {
+  display.value = eval(display.value);
+}
